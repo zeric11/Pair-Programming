de08334762e3d6ac631b3b7cd089a019da1597dd
Eric Zmitrovich <ezmitrovich11@gmail.com>
2021-07-14 21:07:34 -0700

    Kata Commit #2

--- NumberToLCD/src/main/java/edu/pdx/cs410J/dylaneric/Kata.java
+++ NumberToLCD/src/main/java/edu/pdx/cs410J/dylaneric/Kata.java
@@ -8,28 +8,58 @@ package edu.pdx.cs410J.dylaneric;
  */
 public class Kata {
 
-    private static String[] output;
+    private static StringBuffer[] output;
 
-    public static final String ZERO_TOP = " _  ", ZERO_MID = "|  | ", ZERO_BOT = "|_| ", ONE_TOP = "    ",
-            ONE_MID = " | ", ONE_BOT = " | ", TWO_TOP = " _  ", TWO_MID = "  | ", TWO_BOT = "|_  ",
-            THREE_TOP = "_  ", THREE_MID = "_| ", THREE_BOT = "_| ", FOUR_TOP = "    ", FOUR_MID = "|_| ", FOUR_BOT = "  | ",
-            FIVE_TOP = " _  ", FIVE_MID = "|   ", FIVE_BOT = " _| ", SIX_TOP = " _  ", SIX_MID = "|_  ", SIX_BOT = "|_| ",
-            SEV_TOP = "_  ", SEV_MID = " | ", SEV_BOT = " | ", EIGHT_TOP = " _ ", EIGHT_MID = "|_| ", EIGHT_BOT = "|_| ",
-            NINE_TOP = " _  ", NINE_MID = "|_| ", NINE_BOT = " _| ";
+    static String[][] digitItems = {{" _  ", "| | ", "|_| "},
+                                    {"    ", " |  ", " |  "},
+                                    {" _  ", " _| ", "|_  "},
+                                    {" _  ", " _| ", " _| "},
+                                    {"    ", "|_| ", "  | "},
+                                    {" _  ", "|_  ", " _| "},
+                                    {" _  ", "|_  ", "|_| "},
+                                    {"_   ", " |  ", " |  "},
+                                    {" _  ", "|_| ", "|_| "},
+                                    {" _  ", "|_| ", " _| "}};
 
 
     public static void main(String[] args) {
+        if(args == null || args.length == 0) {
+            args = new String[1];
+            args[0] = "0123456789";
+        }
+        int[] input = getIntegerArray(args[0]);
+        output = new StringBuffer[3];
+        for(int i = 0; i < output.length; ++i) {
+            output[i] = new StringBuffer();
+        }
+        for(int current : input) {
+            add(current);
+        }
 
-        int input = Integer.parseInt(args[0]);
+        print();
 
         System.exit(1);
     }
 
 
     public static void add(int input) {
-
+        for(int i = 0; i < 3; ++i) {
+            output[i].append(digitItems[input][i]);
+        }
     }
 
-    public static void add0() {
+    public static void print() {
+        for(int i = 0; i < 3; ++i) {
+            System.out.println(output[i]);
+        }
     }
+
+    private static int[] getIntegerArray(String input) {
+        int[] toReturn = new int[input.length()];
+        for(int i = 0; i < input.length(); ++i) {
+            toReturn[i] = input.charAt(i) - 48;
+        }
+        return toReturn;
+    }
+
 }
