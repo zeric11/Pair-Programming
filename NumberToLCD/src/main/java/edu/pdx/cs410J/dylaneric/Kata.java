d0577f3dd229a90cf9ed14c86f4104a6cbae9e6f
Dylan Holmes <holmes.dylan@hotmail.com>
2021-07-14 20:20:06 -0700

    - Kata Driving Commit #1

---
+++ NumberToLCD/src/main/java/edu/pdx/cs410J/dylaneric/Kata.java
@@ -0,0 +1,35 @@
+package edu.pdx.cs410J.dylaneric;
+
+/**
+ * A class for getting started with a code kata
+ * <p>
+ * Use IntelliJ's "Refactor | Rename..." command to change the name of this
+ * class (and its tests).
+ */
+public class Kata {
+
+    private static String[] output;
+
+    public static final String ZERO_TOP = " _  ", ZERO_MID = "|  | ", ZERO_BOT = "|_| ", ONE_TOP = "    ",
+            ONE_MID = " | ", ONE_BOT = " | ", TWO_TOP = " _  ", TWO_MID = "  | ", TWO_BOT = "|_  ",
+            THREE_TOP = "_  ", THREE_MID = "_| ", THREE_BOT = "_| ", FOUR_TOP = "    ", FOUR_MID = "|_| ", FOUR_BOT = "  | ",
+            FIVE_TOP = " _  ", FIVE_MID = "|   ", FIVE_BOT = " _| ", SIX_TOP = " _  ", SIX_MID = "|_  ", SIX_BOT = "|_| ",
+            SEV_TOP = "_  ", SEV_MID = " | ", SEV_BOT = " | ", EIGHT_TOP = " _ ", EIGHT_MID = "|_| ", EIGHT_BOT = "|_| ",
+            NINE_TOP = " _  ", NINE_MID = "|_| ", NINE_BOT = " _| ";
+
+
+    public static void main(String[] args) {
+
+        int input = Integer.parseInt(args[0]);
+
+        System.exit(1);
+    }
+
+
+    public static void add(int input) {
+
+    }
+
+    public static void add0() {
+    }
+}
